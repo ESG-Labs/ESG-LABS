60b22258524db3d84f1bf1f40e1dea010bf43a3e
GabrielCereals <gabrielalejandro.br99@gmail.com>
2021-02-14 05:15:21 -0600

    working on the navigation bar

--- esg-landing-site-2021/src/skeleton-components/navigationbar.js
+++ esg-landing-site-2021/src/skeleton-components/navigationbar.js
@@ -2,16 +2,16 @@ import React from 'react';
 import logo from './resources/esg-logo.PNG';
 import './navigationbar.css'
 function NavBar(){
-    return(
-        <div className="navigationBar">
-            <img src={logo} className="logo" alt="logo" />
-            <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Our Work</li>
-                <li>Contact</li>
-            </ul>
-        </div>
+    return(   
+        <div className="navBar">
+            <img className="nbLogo" src={logo} alt="esg-labs" width="64" height="64" />
+            <ul className="menuanks">
+                <a className="menuItem">Home</a>
+                <a className="menuItem">About</a>
+                <a className="menuItem">Our Work</a>
+                <a className="menuItem">Contact</a>
+            </ul> 
+        </div>                    
     );
 }
 
